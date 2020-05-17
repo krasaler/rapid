e971476905c3ff95477f4c6658c8726378a4fc38
Alexandr Krasny <php4.drvn@gmail.com>
2020-05-17 10:52:46 +0300

    fix: fix bug

--- mod.ts
+++ mod.ts
@@ -1,11 +1,10 @@
+import { loadConfig } from "./common.ts";
+import Router from "./engine/router.ts";
+import { initPlugins } from "./helper/plugin.ts";
 export * from "./common.ts";
 export * from "./helper/request.ts";
 export * from "./helper/plugin.ts";
 export * from "./helper/event.ts";
-import { loadConfig } from "./common.ts";
-import Router from "./engine/router.ts";
-import { initPlugins } from "./helper/plugin.ts";
-
 export const start = async (cfg: any) => {
   loadConfig(cfg);
   await initPlugins();
