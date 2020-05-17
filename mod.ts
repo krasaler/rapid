c2992c5f2611a900c179f681a59030db8fd864a0
Alexandr Krasny <php4.drvn@gmail.com>
2020-05-17 10:36:56 +0300

    feat: start move to mod.ts

--- mod.ts
+++ mod.ts
@@ -2,3 +2,13 @@ export * from "./common.ts";
 export * from "./helper/request.ts";
 export * from "./helper/plugin.ts";
 export * from "./helper/event.ts";
+import { loadConfig } from "./common.ts";
+import Router from "./engine/router.ts";
+import { initPlugins } from "./helper/plugin.ts";
+
+export const start = async (cfg: any) => {
+  loadConfig(cfg);
+  await initPlugins();
+  const router = new Router();
+  router.start();
+};
